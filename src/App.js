e96d424c8b75326ffb95df83b330e43caf838c86
jamesjc78 <jcjamesarcilla@gmail.com>
2022-01-13 17:48:25 +0800

    link home to leave page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import LoginForm from "./components/login/login";
 import Home from "./components/home/home";
+import Leave from "./components/leave/leave";
 import { withRouter } from "./withRouter";
 
 const emailRegex = RegExp(
   /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 );
+
+// sorting data
+const mySort = (arr, sortBy) => {
+  arr.sort((a, b) => (a[sortBy] > b[sortBy] ? 1 : -1));
+};
 
 const loginValid = (states) => {
   let valid = true;
@@ -33,6 +39,7 @@ class App extends React.Component {
     position: null,
     password: null,
     employees: [
+      // list of employees
       {
         name: "J.C. James Arcilla",
         email: "jla@noaya.no",
@@ -51,7 +58,34 @@ class App extends React.Component {
         position: "Software Developer",
         password: null,
       },
-    ], // list of employees
+    ],
+    leaves: [
+      // list of leaves
+      {
+        email: "kmg@noaya.no",
+        date: Date(2022, 1, 12),
+        type: "Sick",
+        notes: "Headache and Fever",
+      },
+      {
+        email: "kmg@noaya.no",
+        date: Date(2022, 6, 12),
+        type: "Vacation",
+        notes: "N/A",
+      },
+      {
+        email: "kmg@noaya.no",
+        date: Date(2022, 1, 10),
+        type: "Emergency",
+        notes: "Family Emergency",
+      },
+      {
+        email: "jla@noaya.no",
+        date: Date(2022, 1, 12),
+        type: "Sick",
+        notes: "Headache and Fever",
+      },
+    ],
     loginError: {
       userError: "", // login error messages
       passwordError: "",
@@ -93,6 +127,19 @@ class App extends React.Component {
   };
 
   // Home Handlers
+  handleUserRowClick = (event) => {
+    event.preventDefault();
+    console.log(event);
+    // const { name } = event.target;
+    // const leaves = [];
+    // this.state.leaves.map((leave) => {
+    //   if (leave.email == name) {
+    //     leaves.push(leave);
+    //   }
+    // });
+    // mySort(leaves, "date");
+    this.props.navigate(`/user/${event.target.innerText}`);
+  };
 
   render() {
     return (
@@ -107,7 +154,13 @@ class App extends React.Component {
             />
           }
         />
-        <Route path="/user" element={<Home states={this.state} />} />
+        <Route
+          path="/user"
+          element={
+            <Home states={this.state} onRowClick={this.handleUserRowClick} />
+          }
+        />
+        <Route path="/user/:email" element={<Leave states={this.state} />} />
       </Routes>
     );
   }
